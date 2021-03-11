573a55dcddb15015f6992612beb8c7e31948122f
Lucas Lennon <lucasespir@gmail.com>
2021-03-11 14:40:26 -0300

    Add Common Layout to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,40 +1,12 @@
-import Link from 'next/link';
-
 import { ReactElement } from 'react';
-import { useTranslation } from 'react-i18next';
-import { useQuery } from 'react-query';
 
-import { Box, Link as UnitedLink, Flex, Text } from '@olist/united';
-
-import ExampleService from '~common/service/example';
+import CommonLayout from '~common/layouts/CommonLayout';
 
 const HomePage = (): ReactElement => {
-  const { t } = useTranslation('home');
-  const { data, error, isLoading } = useQuery(
-    'repoData',
-    async () => ExampleService.commonRequest(),
-    {
-      refetchOnMount: false,
-      refetchOnWindowFocus: false,
-    }
-  );
-
   return (
-    <Flex flexDirection="column">
-      <div>{t('welcomeMessage')}</div>
-      <div>
-        {t('{{date, dateTimeFormatAsShort}}', {
-          date: new Date(),
-        })}
-      </div>
-      {error && 'error found'}
-      {isLoading ? 'loading content' : <Text>{data.name}</Text>}
-      <Box>
-        <Link href="/another">
-          <UnitedLink>Another Page</UnitedLink>
-        </Link>
-      </Box>
-    </Flex>
+    <CommonLayout>
+      <div>body</div>
+    </CommonLayout>
   );
 };
 
