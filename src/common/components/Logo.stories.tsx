93f9177cc8f924e75551ff6ad4e2d6822b4e12db
Lucas Lennon <lucasespir@gmail.com>
2021-03-12 16:18:46 -0300

    Add header and logo component

---
+++ src/common/components/Logo.stories.tsx
@@ -0,0 +1,30 @@
+import { ReactElement } from 'react';
+
+import { Flex, Box } from '@olist/united';
+
+import Logo from './Logo';
+
+export const Example = (): ReactElement => {
+  return (
+    <Flex>
+      <Box>
+        <Logo variation="vertical" />
+      </Box>
+      <Box>
+        <Logo />
+      </Box>
+    </Flex>
+  );
+};
+
+export default {
+  title: 'Common/Components/Logo',
+  component: Logo,
+  parameters: {
+    docs: {
+      description: {
+        component: "It's the project logo, it combines the olist store logo to the common slogan",
+      },
+    },
+  },
+};
