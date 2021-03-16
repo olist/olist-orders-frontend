ccdbd9910cfb104e9cacce67f26eb0e19a6117f8
LucasLennon <lucasespir@gmail.com>
2021-03-16 16:54:14 -0300

    Add Footer to Layout

--- src/common/layouts/Layout.tsx
+++ src/common/layouts/Layout.tsx
@@ -3,6 +3,9 @@ import { ReactElement } from 'react';
 import styled from 'styled-components';
 
 import { Layout as UiCommonsLayout } from '@olist/ui-commons/dist/ui-commons.cjs';
+import { Flex } from '@olist/united';
+
+import Footer from '~common/components/Footer';
 
 interface Layout {
   children: JSX.Element[] | JSX.Element;
@@ -13,12 +16,23 @@ export const Container = styled(UiCommonsLayout.Container).attrs({
   px: 3,
 })`
   background: ${themeGet('colors.foundation.shadeWhite')};
+  display: flex;
+  flex-direction: column;
   min-height: 100vh;
   max-width: 1440px;
 `;
 
 const Layout = ({ children }: Layout): ReactElement => {
-  return <Container>{children}</Container>;
+  return (
+    <Container>
+      <Flex flexDirection="column" flexGrow={1}>
+        {children}
+      </Flex>
+      <Flex width={1} justifyContent="center">
+        <Footer />
+      </Flex>
+    </Container>
+  );
 };
 
 export default Layout;
