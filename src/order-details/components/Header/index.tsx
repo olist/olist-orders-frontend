fddd307ba4be8360d1f24adb2f23440f81b46d43
LucasLennon <lucasespir@gmail.com>
2021-03-24 17:48:33 -0300

    Add button to order details header

--- src/order-details/components/Header/index.tsx
+++ src/order-details/components/Header/index.tsx
@@ -1,11 +1,24 @@
+import { useRouter } from 'next/router';
+
+import { useTranslation } from 'react-i18next';
+
 import { Flex } from '@olist/united';
 
+import { ButtonWithoutBorder } from '~/common/components/Button/index.styles';
 import Logo from '~/common/components/Logo';
 
 const Header = () => {
+  const { t } = useTranslation('orderDetailsHeader');
+  const router = useRouter();
+
+  const handleClick = () => router.push('/');
+
   return (
-    <Flex mt={8}>
+    <Flex mt={8} justifyContent="space-between">
       <Logo variation="horizontal" />
+      <ButtonWithoutBorder icon="search-outline" onClick={handleClick}>
+        {t('headerButtonSearch')}
+      </ButtonWithoutBorder>
     </Flex>
   );
 };
