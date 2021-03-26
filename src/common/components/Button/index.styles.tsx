83b4cc531fe450468c21ee851499079e4773bb75
LucasLennon <lucasespir@gmail.com>
2021-03-26 13:30:19 -0300

    Add ButtonRounded to Button styles

--- src/common/components/Button/index.styles.tsx
+++ src/common/components/Button/index.styles.tsx
@@ -5,3 +5,7 @@ import { Button } from '@olist/united';
 export const ButtonBorderless = styled(Button)`
   border: none;
 `;
+
+export const ButtonRounded = styled(Button)`
+  border-radius: 6rem;
+`;
